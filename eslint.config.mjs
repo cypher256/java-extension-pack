a93d82a59a90969a6936c050a120cc2ef9a25a6a
cypher256 <kashihara@gmail.com>
2024-10-19 16:59:16 +0900

    Update tsconfig.json and .vscode/settings.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,19 @@
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
+// Use mjs because ts is not yet officially supported
+// https://eslint.org/docs/latest/use/configure/configuration-files#typescript-configuration-files
 export default [
     eslint.configs.recommended,
     ...tseslint.configs.recommended,
     {
         ignores: ["dist"],
     }, {
-    rules: {
-        "@typescript-eslint/no-explicit-any": ["off"],
-        "@typescript-eslint/no-namespace": ["off"],
-        "@typescript-eslint/no-unused-vars": ["off"],
-        "no-undef": ["off"],
-    },
-}];
+        rules: {
+            "@typescript-eslint/no-explicit-any": ["off"],
+            "@typescript-eslint/no-namespace": ["off"],
+            "@typescript-eslint/no-unused-vars": ["off"],
+            "no-undef": ["off"],
+        },
+    }
+];
