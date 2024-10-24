5559f6103775e3d81c7c360dcedfaeaa837c3b6e
cypher256 <kashihara@gmail.com>
2024-10-24 22:53:22 +0900

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,18 @@
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import eslintJs from "@eslint/js";
+import eslintTs from "typescript-eslint";
 
 // Use mjs because ts is not yet officially supported
 // https://eslint.org/docs/latest/use/configure/configuration-files#typescript-configuration-files
 export default [
-    eslint.configs.recommended,
-    ...tseslint.configs.recommended,
+    eslintJs.configs.recommended,
+    ...eslintTs.configs.recommended,
     {
         ignores: ["dist"],
     }, {
         rules: {
-            "@typescript-eslint/no-explicit-any": ["off"],
-            "@typescript-eslint/no-namespace": ["off"],
-            "@typescript-eslint/no-unused-vars": ["off"],
-            "no-undef": ["off"],
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-namespace": "off",
+            "@typescript-eslint/no-unused-vars": "off",
         },
     }
 ];
